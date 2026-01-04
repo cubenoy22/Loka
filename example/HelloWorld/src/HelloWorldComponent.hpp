64db256059479f3f818af2f1542d267df2fdfd48
cubenoy22 <17539161+cubenoy22@users.noreply.github.com>
2026-01-04 13:12:16 +0900

    Initialize HelloWorld message state in prepareNode

--- example/HelloWorld/src/HelloWorldComponent.hpp
+++ example/HelloWorld/src/HelloWorldComponent.hpp
@@ -21,13 +21,13 @@ namespace helloworld
     HelloWorldNode(const HelloWorldProps &p)
         : declara::core::scene::GroupNodeBase<HelloWorldProps>(HelloWorldProps(p)), props(p), message_(0) {}
 
+    virtual void prepareNode(declara::core::scene::NodeComposition &c)
+    {
+      message_ = &c.useState<std::string>("Hello, Loka!");
+    }
+
     virtual void composeNode(declara::core::scene::NodeComposition &c)
     {
-      if (!message_)
-      {
-        message_ = &c.useState<std::string>("Hello, Loka!");
-      }
-
       using namespace declara::app;
       c.declare(
           VStack()
