00355b1a3a584788474be5b867080da3c0fe8bbb
cubenoy22 <17539161+cubenoy22@users.noreply.github.com>
2026-02-09 20:29:40 +0900

    Fix modern macOS SDK compatibility and ARC entrypoint handling

--- apple/macos/src/MacObjCCompat.hpp
+++ apple/macos/src/MacObjCCompat.hpp
@@ -3,6 +3,7 @@
 
 #import <AvailabilityMacros.h>
 
+#if !defined(__OBJC2__) || !__OBJC2__
 #ifndef NSINTEGER_DEFINED
 typedef int NSInteger;
 typedef unsigned int NSUInteger;
@@ -13,9 +14,12 @@ typedef unsigned int NSUInteger;
 typedef float CGFloat;
 #define CGFLOAT_TYPE 1
 #endif
+#endif
 
+#if !defined(MAC_OS_X_VERSION_MAX_ALLOWED) || (MAC_OS_X_VERSION_MAX_ALLOWED < 1070)
 #ifndef NSApplicationActivationPolicyRegular
 #define NSApplicationActivationPolicyRegular 0
+#endif
 #endif
 
 #endif // LOKA_MAC_OBJC_COMPAT_HPP
--- example/SimpleViewer/src/MacOSMain.mm
+++ example/SimpleViewer/src/MacOSMain.mm
@@ -7,9 +7,21 @@ int main(int argc, char **argv)
   (void)argc;
   (void)argv;
 
+#if defined(__has_feature)
+#if __has_feature(objc_arc)
+  @autoreleasepool {
+    loka::platform::RunApp<MyAppConfig>();
+  }
+#else
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   loka::platform::RunApp<MyAppConfig>();
   [pool release];
+#endif
+#else
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  loka::platform::RunApp<MyAppConfig>();
+  [pool release];
+#endif
 
   return 0;
 }
