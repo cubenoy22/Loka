8a57b200a17de5615579f74c53df4dac1c4be157
cubenoy22 <17539161+cubenoy22@users.noreply.github.com>
2026-04-29 18:54:53 +0900

    chore: format

--- example/MineSweeper/src/MyAppConfig.hpp
+++ example/MineSweeper/src/MyAppConfig.hpp
@@ -6,13 +6,12 @@
 #include "app/core/WindowDefinition.hpp"
 #include "MainNode.hpp"
 
-class MyAppConfig : public AppConfigurable
-{
+class MyAppConfig : public AppConfigurable {
 public:
-  explicit MyAppConfig(PlatformContext *ctx) : AppConfigurable(ctx) {}
+  explicit MyAppConfig(PlatformContext *ctx) : AppConfigurable(ctx) {
+  }
 
-  virtual void compose(AppComposition &c)
-  {
+  virtual void compose(AppComposition &c) {
     c << WindowDef(WindowProps()
                        .frame(20, 20, 220, 240)
                        .scene(loka::app::scene::NodeDefinition<minesweeper::MainProps, minesweeper::MainNode>())
