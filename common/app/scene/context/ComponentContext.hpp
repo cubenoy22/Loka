d0ab01e965ab3dd90ff9b30d450dcebbadc10071
cubenoy22 <17539161+cubenoy22@users.noreply.github.com>
2026-05-07 00:27:24 +0900

    format: ComponentContext

--- common/app/scene/context/ComponentContext.hpp
+++ common/app/scene/context/ComponentContext.hpp
@@ -22,28 +22,103 @@ namespace loka
       class ComponentContext
       {
       public:
-        ComponentContext() : parent_(0), owner_(0), stateOwner_(0), boundary_(0), platformController_(0), scene_(0), window_(0), composition_(0), dirtyFlags_(NODE_DIRTY_NONE) {}
+        ComponentContext()
+            : parent_(0),
+              owner_(0),
+              stateOwner_(0),
+              boundary_(0),
+              platformController_(0),
+              scene_(0),
+              window_(0),
+              composition_(0),
+              dirtyFlags_(NODE_DIRTY_NONE)
+        {
+        }
         explicit ComponentContext(ComponentContext *parent)
-            : parent_(parent), owner_(0), stateOwner_(0), boundary_(0), platformController_(0), scene_(0), window_(0), composition_(0), dirtyFlags_(NODE_DIRTY_NONE) {}
+            : parent_(parent),
+              owner_(0),
+              stateOwner_(0),
+              boundary_(0),
+              platformController_(0),
+              scene_(0),
+              window_(0),
+              composition_(0),
+              dirtyFlags_(NODE_DIRTY_NONE)
+        {
+        }
 
-        ComponentContext *parent() const { return parent_; }
-        void setParent(ComponentContext *parent) { parent_ = parent; }
-        Node *owner() const { return owner_; }
-        void setOwner(Node *owner) { owner_ = owner; }
-        IStateOwner *stateOwner() const { return stateOwner_; }
-        void setStateOwner(IStateOwner *owner) { stateOwner_ = owner; }
-        BoundaryNode *boundary() const { return boundary_; }
-        void setBoundary(BoundaryNode *boundary) { boundary_ = boundary; }
-        IPlatformController *platformController() const { return platformController_; }
-        void setPlatformController(IPlatformController *controller) { platformController_ = controller; }
-        Scene *scene() const { return scene_; }
-        void setScene(Scene *scene) { scene_ = scene; }
-        ::Window *window() const { return window_; }
-        void setWindow(::Window *window) { window_ = window; }
-        NodeComposition *composition() const { return composition_; }
-        void setComposition(NodeComposition *comp) { composition_ = comp; }
-        NodeDirtyFlags dirtyFlags() const { return dirtyFlags_; }
-        void setDirtyFlags(NodeDirtyFlags flags) { dirtyFlags_ = flags; }
+        ComponentContext *parent() const
+        {
+          return parent_;
+        }
+        void setParent(ComponentContext *parent)
+        {
+          parent_ = parent;
+        }
+        Node *owner() const
+        {
+          return owner_;
+        }
+        void setOwner(Node *owner)
+        {
+          owner_ = owner;
+        }
+        IStateOwner *stateOwner() const
+        {
+          return stateOwner_;
+        }
+        void setStateOwner(IStateOwner *owner)
+        {
+          stateOwner_ = owner;
+        }
+        BoundaryNode *boundary() const
+        {
+          return boundary_;
+        }
+        void setBoundary(BoundaryNode *boundary)
+        {
+          boundary_ = boundary;
+        }
+        IPlatformController *platformController() const
+        {
+          return platformController_;
+        }
+        void setPlatformController(IPlatformController *controller)
+        {
+          platformController_ = controller;
+        }
+        Scene *scene() const
+        {
+          return scene_;
+        }
+        void setScene(Scene *scene)
+        {
+          scene_ = scene;
+        }
+        ::Window *window() const
+        {
+          return window_;
+        }
+        void setWindow(::Window *window)
+        {
+          window_ = window;
+        }
+        NodeComposition *composition() const
+        {
+          return composition_;
+        }
+        void setComposition(NodeComposition *comp)
+        {
+          composition_ = comp;
+        }
+        NodeDirtyFlags dirtyFlags() const
+        {
+          return dirtyFlags_;
+        }
+        void setDirtyFlags(NodeDirtyFlags flags)
+        {
+          dirtyFlags_ = flags;
+        }
 
       private:
         friend class ComposableNode;
