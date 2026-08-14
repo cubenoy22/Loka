b2db281fac7754b49db7775990b7989af55fc3d9
Lihi-AI (Bot) <294707187+Lihi-AI@users.noreply.github.com>
2026-08-15 00:01:42 +0900

    Inject MineSweeper board seed through props

--- example/MineSweeper/src/MacOSMain.mm
+++ example/MineSweeper/src/MacOSMain.mm
@@ -1,6 +1,22 @@
 #include "app/bootstrap/RunApp.hpp"
 #include "MyAppConfig.hpp"
 #include <Foundation/Foundation.h>
+#include <ctime>
+
+namespace
+{
+  class ProductionAppConfig : public MyAppConfig
+  {
+  public:
+    explicit ProductionAppConfig(PlatformContext *context)
+        : MyAppConfig(
+              context,
+              minesweeper::MainProps(
+                  static_cast<unsigned long>(std::time(0))))
+    {
+    }
+  };
+}
 
 int main(int argc, char **argv)
 {
@@ -8,7 +24,7 @@ int main(int argc, char **argv)
   (void)argv;
 
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  int result = loka::platform::RunApp<MyAppConfig>();
+  int result = loka::platform::RunApp<ProductionAppConfig>();
   // Leopard/PPC may crash while popping the outermost process-lifetime pool
   // during shutdown. The process is exiting here anyway, so let the OS reclaim
   // it instead of releasing the pool explicitly.
--- example/MineSweeper/src/main.cpp
+++ example/MineSweeper/src/main.cpp
@@ -1,19 +1,35 @@
 #include "app/bootstrap/RunApp.hpp"
 #include "MyAppConfig.hpp"
+#include <ctime>
+
+namespace
+{
+  class ProductionAppConfig : public MyAppConfig
+  {
+  public:
+    explicit ProductionAppConfig(PlatformContext *context)
+        : MyAppConfig(
+              context,
+              minesweeper::MainProps(
+                  static_cast<unsigned long>(std::time(0))))
+    {
+    }
+  };
+}
 
 #if defined(_WIN32) || defined(WIN32)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
   (void)hPrevInstance;
   (void)lpCmdLine;
-  return loka::platform::RunApp<MyAppConfig>(hInstance, nCmdShow);
+  return loka::platform::RunApp<ProductionAppConfig>(hInstance, nCmdShow);
 }
 #elif defined(LOKA_RETRO68)
 int main(int argc, char **argv)
 {
   (void)argc;
   (void)argv;
-  return loka::platform::RunApp<MyAppConfig>();
+  return loka::platform::RunApp<ProductionAppConfig>();
 }
 #else
 #error Unsupported platform for MineSweeper main.cpp
